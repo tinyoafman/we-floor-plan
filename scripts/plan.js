9f2cea7e7766037830042f1067ee2f5f929160b2
Thomas Sheader <thomas.sheader@dwp.gsi.gov.uk>
2018-06-27 13:48:51 +0100

    TS initial commit

--- scripts/plan.js
+++ scripts/plan.js
@@ -1,4 +1,10 @@
 function showPlanElement(val){
+  // total hack but...
+  var rooms = document.getElementsByClassName("clearable")
+  console.log(rooms.length)
+  for (var i=0;i<rooms.length;i++) {
+    rooms[i].style.backgroundColor="#dad9db"
+  }
   document.getElementById(val).style.backgroundColor="green"
   console.log("showing plan element: "+val)
 }
