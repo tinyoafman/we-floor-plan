85c3d52d5677561eafc42b37a5adc6068f0a6885
Thomas Sheader <thomas.sheader@dwp.gsi.gov.uk>
2018-06-28 13:25:27 +0100

    TS add extra column

--- scripts/plan.js
+++ scripts/plan.js
@@ -1,3 +1,22 @@
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scroll()};
+
+function scroll() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("top-btn").style.display = "block";
+    } else {
+        document.getElementById("top-btn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function gotoTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
 function showPlanElement(type, val){
   console.log("showing plan element: "+val)
 
@@ -9,7 +28,7 @@ function showPlanElement(type, val){
     scrollTop: offset.top-100,
     scrollLeft: offset.left-20
   });
-  
+
   elem.addClass("flash");
 
   setTimeout(function() {
