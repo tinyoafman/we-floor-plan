f45a57f214686d334b406d45b90d9ea54399d53e
Jenkins <jenkins@esa.com>
2018-06-28 14:44:10 +0100

    CK perfect text align

--- scripts/plan.js
+++ scripts/plan.js
@@ -1,4 +1,25 @@
 function showPlanElement(val){
+
+  var rooms = document.getElementsByClassName("colourable")
+
+  for(var i = 0; i < rooms.length; i++){
+    rooms[i].style.backgroundColor="#dad9db"
+  }
+
   document.getElementById(val).style.backgroundColor="green"
-  console.log("showing plan element: "+val)
+}
+
+function showTeamElement(val){
+
+  var rooms = document.getElementsByClassName("colourable")
+
+  for(var i = 0; i < rooms.length; i++){
+    rooms[i].style.backgroundColor="#dad9db"
+  }
+
+  var desks = document.getElementsByClassName(val)
+
+  for(var i = 0; i < desks.length; i++){
+    desks[i].style.backgroundColor="green"
+  }
 }
